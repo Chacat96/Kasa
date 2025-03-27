8a896ae11dd96460500c75a6c893716cc979038f
Charlene CATHERIN <c.catherin@outlook.fr>
2025-03-27 16:00:22 +0100

    ajout de l'état active pour la navigation visuelle

--- src/composant/Header.jsx
+++ src/composant/Header.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { useEffect, useState } from "react";
 import LOGO from "../assets/logo/LOGO.png";
 import  "../style/css/Header.css";
 
 const Header = React.FC = () => {
+    const [activePage, setActivePage] = useState(window.location.pathname);
+
+    useEffect(() => {
+        const handleLocationChange = () => setActivePage(window.location.pathname);
+        window.addEventListener("popstate", handleLocationChange);
+        return () => window.removeEventListener("popstate", handleLocationChange);
+    }, []);
+
     return (
         <nav className="header">
             <img src={LOGO} alt="logo kasa" className="header__logo-header"/>
             <div className="header__nav-header">
-                <a href="/">Accueil</a>
-                <a href="/about">A propos</a>
+            <a href="/" className={activePage === "/" ? "active" : ""}>Accueil</a>
+                <a href="/about" className={activePage === "/about" ? "active" : ""}>À propos</a>
             </div>
         </nav>
     );
