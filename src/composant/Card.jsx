72541ef7048341495b8d20e142d8e4f13df0cde3
Charlene CATHERIN <c.catherin@outlook.fr>
2025-03-27 15:01:45 +0100

    composant card et dropdow créé

---
+++ src/composant/Card.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../style/css/Card.css";
+
+const Card = React.FC = () => {
+    return (
+        <div className="Card">
+            <h1>card</h1>
+        </div>
+    );
+};
+
+export default Card;
