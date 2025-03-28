9ebb317860f03b0beb4182526773aa27c679048f
Charlene CATHERIN <c.catherin@outlook.fr>
2025-03-28 10:59:17 +0100

    page annonce créé, en cours

---
+++ src/pages/Annonce.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import  "../style/css/Annonce.css";
+
+const Annonce = () => {
+    return (
+       <div className="annonce">
+       
+       </div>
+    );
+};
+
+export default Annonce;
