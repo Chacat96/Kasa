9babcbdb508c848be73fe409404a9e6b842cf772
Charlene CATHERIN <c.catherin@outlook.fr>
2025-03-28 17:48:54 +0100

    chemin page annonce et integration des données

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Layout from './composant/Layout';
 import Accueil from './pages/Accueil';
+import Annonce from './pages/Annonce';
 import About from './pages/About';
 import Error from './pages/Error';
 
@@ -11,6 +12,7 @@ function App() {
      
       <Routes>
         <Route path="/" element={<Layout><Accueil /></Layout>} />
+        <Route path="/annonce/:id" element={<Layout><Annonce /></Layout>} />
         <Route path="/about" element={<Layout><About /></Layout>} />
         <Route path="*" element={<Layout><Error /></Layout>} />
       </Routes>
--- src/pages/Annonce.jsx
+++ src/pages/Annonce.jsx
@@ -1,12 +1,22 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import  "../style/css/Annonce.css";
+import data from "../json/data.json";
+import Carrousel from "../composant/Carrousel";
 
 const Annonce = () => {
+
+const { id } = useParams(); 
+const annonce = data.find(item => item.id === id); 
+
     return (
-       <div className="annonce">
-       
-       </div>
+        <div className="annonce">
+            <h1>{annonce.title}</h1>
+            <Carrousel img={annonce.pictures} />  
+            <p>{annonce.description}</p>
+        </div>
     );
+
 };
 
 export default Annonce;
