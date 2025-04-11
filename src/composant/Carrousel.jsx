9a320260d2c88e1e6f468a9f4dd5ba25a99574de
Charlene CATHERIN <c.catherin@outlook.fr>
2025-04-11 13:42:39 +0200

    nombre d'image ajouté au carrousel

--- src/composant/Carrousel.jsx
+++ src/composant/Carrousel.jsx
@@ -6,15 +6,20 @@ import rightArrow from "../assets/icon/arrow_right.png";
 const Carrousel = ({img}) => {
     const [index, setIndex] = useState(0);
 
+    const nextImage = () => setIndex((index + 1) % img.length);
+    const prevImage = () => setIndex((index - 1 + img.length) % img.length);
+   
+    //Rendu d'une image ou plusieurs dans le carrousel
     if (img.length === 1) {
-        return <img src={img[0]} alt="Annonce" className="carrousel__image" />;
-    }
-
-    const nextImage = () => setIndex((index + 1) % images.length);
-    const prevImage = () => setIndex((index - 1 + images.length) % images.length);
-    return (
+        return <img src={img[0]} alt="image de l'annonce" className="carrousel__image" />;
+    } else {
+        return (
        <div className="carrousel">
             <img src={img[index]} alt={`image ${index + 1}`} className="carrousel__image" />
+       
+       <div className="carrousel__nbr">
+        <p>{index + 1}/{img.length}</p>
+       </div>
         <div className="carrousel__arrow">
             <img src={leftArrow} alt="left arrow" onClick={prevImage}/>
             <img src={rightArrow} alt="right arrow" onClick={nextImage}/>
@@ -22,6 +27,10 @@ const Carrousel = ({img}) => {
         
        </div>
     );
+    }
+
+   
+   
 };
 
 export default Carrousel;
